07ae32574df288c0493b30e45d672bbc9275cc76
faizaanshamsi <faizaan2000@gmail.com>
2014-11-17 11:21:58 -0500

    uncomment

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -50,53 +50,53 @@ class Bot
 
     puts 'Visiting new product page'
     s.visit '/wp-admin/post-new.php?post_type=product'
+    
+    puts "Filling in title with #{data[:title]}"
+    s.fill_in 'post_title', with: data[:title]
+
+    puts "Filling in price with #{data[:price]}"
+    s.fill_in '_regular_price', with: data[:price]
+    # check location
+    # check city
+    # check room
+    # check subroom
+    puts "Filling in description with #{data[:description]}"
+    s.within_frame 'content_ifr' do
+      s.execute_script("document.getElementsByTagName('p')[0].innerHTML = \"#{data[:description]}\";")
+    end
+
+    puts "Filling in seller name with #{data[:owner_name]}"
+    s.fill_in 'pods_meta_seller_name', with: data[:owner_name]
+
+    if data[:seller]
+      puts "Selecting #{data[:seller]} from available sellers"
+      s.select data[:seller], from: 'pods_meta_seller'
+    end
+
+    if data[:featured]
+      puts "Checking off Featured"
+      s.find('a.edit-catalog-visibility', text: "Edit").click
+      s.find('input#_featured').set(true)
+      s.find('a.save-post-visibility', text: "OK").click
+    end
+    # check hot deal if hot deal
+    if data[:hot_deals]
+      puts "Checking off Hot Deals"
+      s.check 'pods_meta_hotdeal'
+    end
     #
-    # puts "Filling in title with #{data[:title]}"
-    # s.fill_in 'post_title', with: data[:title]
-    #
-    # puts "Filling in price with #{data[:price]}"
-    # s.fill_in '_regular_price', with: data[:price]
-    # # check location
-    # # check city
-    # # check room
-    # # check subroom
-    # puts "Filling in description with #{data[:description]}"
-    # s.within_frame 'content_ifr' do
-    #   s.execute_script("document.getElementsByTagName('p')[0].innerHTML = \"#{data[:description]}\";")
-    # end
-    #
-    # puts "Filling in seller name with #{data[:owner_name]}"
-    # s.fill_in 'pods_meta_seller_name', with: data[:owner_name]
-    #
-    # if data[:seller]
-    #   puts "Selecting #{data[:seller]} from available sellers"
-    #   s.select data[:seller], from: 'pods_meta_seller'
-    # end
-    #
-    # if data[:featured]
-    #   puts "Checking off Featured"
-    #   s.find('a.edit-catalog-visibility', text: "Edit").click
-    #   s.find('input#_featured').set(true)
-    #   s.find('a.save-post-visibility', text: "OK").click
-    # end
-    # # check hot deal if hot deal
-    # if data[:hot_deals]
-    #   puts "Checking off Hot Deals"
-    #   s.check 'pods_meta_hotdeal'
-    # end
-    # #
-    # puts "Filling in additional item attributes"
-    # s.find('li.attribute_tab').click
-    # data[:attributes].each do |k, v|
-    #   s.find('button.add_attribute').click
-    #   puts "  Setting item's #{k} to be #{v}"
-    #   #set the name
-    #   s.all('input.attribute_name').last.set(k)
-    #   #set the value
-    #   s.all('.product_attributes textarea').last.set(v)
-    #   #click the visible button
-    #   s.all('.product_attributes input.checkbox').last.set(true)
-    # end
+    puts "Filling in additional item attributes"
+    s.find('li.attribute_tab').click
+    data[:attributes].each do |k, v|
+      s.find('button.add_attribute').click
+      puts "  Setting item's #{k} to be #{v}"
+      #set the name
+      s.all('input.attribute_name').last.set(k)
+      #set the value
+      s.all('.product_attributes textarea').last.set(v)
+      #click the visible button
+      s.all('.product_attributes input.checkbox').last.set(true)
+    end
 
     # create_deal
     create_deal(data[:title], data[:price])
